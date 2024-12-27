e307e7fcb9771774b66c713fd99c8820fb79ef67
tmn1077 <64917060+tmn1077@users.noreply.github.com>
2024-12-27 11:15:34 -0500

    Add files via upload

---
+++ Labs_Verilog/Lab4_5_Sequential_Logic.v
@@ -0,0 +1,38 @@
+\m5_TLV_version 1d: tl-x.org
+\m5
+   
+   // =================================================
+   // Welcome!  New to Makerchip? Try the "Learn" menu.
+   // =================================================
+   
+   //use(m5-1.0)   /// uncomment to use M5 macro library.
+\SV
+   `include "sqrt32.v";
+   // Macro providing required top-level module definition, random
+   // stimulus support, and Verilator config.
+   m5_makerchip_module   // (Expanded in Nav-TLV pane.)
+\TLV
+   $reset = *reset;
+   
+   |comp
+      @1
+         $aa_sq[7:0] = $aa[3:0] ** 2;
+         $bb_sq[7:0] = $bb[3:0] ** 2;
+      @2
+         $cc_sq[8:0] = $aa_sq + $bb_sq;
+      @3
+         $cc[4:0] = sqrt($cc_sq);
+      @4
+         $tot_dist[31:0] =
+            *reset 
+               ? 0 :
+            $valid
+               ? >>1$tot_dist + $cc :
+            //default
+               >>1$tot_dist;
+   
+   // Assert these to end simulation (before Makerchip cycle limit).
+   *passed = *cyc_cnt > 40;
+   *failed = 1'b0;
+\SV
+   endmodule
